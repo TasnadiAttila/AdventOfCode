686c9f07e357ebd34a5101ff30d9918139b1b1b8
TasnadiAttila <70896019+TasnadiAttila@users.noreply.github.com>
2024-12-01 20:55:51 +0100

    Add files via upload

--- 2024/1/1.py
+++ 2024/1/1.py
@@ -4,6 +4,8 @@ file_path = os.path.join(os.path.dirname(__file__), '1.txt')
 with open(file_path, "r") as file:
     lines = [line.strip() for line in file]
 
+#Part 1
+
 left_list = sorted(map(int, [i.split()[0]for i in lines]))
 right_list = sorted(map(int, [i.split()[1] for i in lines]))
 
@@ -11,7 +13,16 @@ distance = 0
 for i in range(len(left_list)):
     distance += abs(right_list[i] - left_list[i])
 
-print(distance)
+print(f'Part 1: {distance}')
+
+#Part 2
+
+collector = 0
+for i in range(len(left_list)):
+    appearance = right_list.count(left_list[i])
+    collector += appearance * left_list[i]
+
+print(f'Part 2: {collector}')
 
 
 
